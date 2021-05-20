e4da20bbd1a236b2dbb07e059e68ff2d1f162169
Danny Paez <dpaez16@yahoo.com>
2021-05-20 11:00:10 -0700

    progress with pop

--- fibheap.py
+++ fibheap.py
@@ -1,31 +1,70 @@
 class FibonacciHeap:
     def __init__(self):
         self.min_node = None
+        self.head = None
         self.size = 0
 
     def push(self, elem, key=None):
         assert elem is not None
+        
         node = FibonacciHeap._Node(elem, key)
 
         if self.size == 0:
             self.min_node = node
+            self.head = node
         else:
-            self._insert(node, self.min_node)
+            self._insert(node, node.prev, self.head)
 
         if node.key < self.min_node.key:
             self.min_node = key
 
         self.size += 1
 
-    def _insert(self, node, root):
+    def _insert(self, node, node_prev, root):
         front = root.next
         root.next = node
-        node.next = front
-        front.prev = node
+        node_prev.next = front
+        front.prev = node_prev
         node.prev = root
 
+    def _remove_node(self, node):
+        front = node.next
+        prev = node.prev
+        prev.next = front
+        front.prev = prev
+
+    def _update_min_node(self):
+        tail = self.head.prev
+        new_min_node = self.head
+        ptr = self.head
+
+        while True:
+            if ptr.key < new_min_node.key:
+                new_min_node = ptr
+
+            if ptr.next.elem == tail.elem:
+                break
+
+            ptr = ptr.next
+
+        return new_min_node
+
     def pop(self):
-        return
+        assert not self.empty()
+
+        elem, key = self.root()
+        if self.size == 1:
+            self.min_node = None
+            self.head = None
+            return
+
+        child = self.min_node.child
+        self._insert(child, child.prev, self.head)
+        self.head = child
+        self._remove_node(self.min_node)
+        self.size -= 1
+        self._update_min_node()
+        return elem, key
 
     def decrease_key(self):
         return
