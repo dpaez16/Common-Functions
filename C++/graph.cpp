33f801f8018d34ae7e31f2f7f1e0d16419313b0d
Danny Paez <dpaez16@yahoo.com>
2021-05-28 15:32:04 -0700

    flipEdge + depending functions

--- C++/graph.cpp
+++ C++/graph.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <functional>
 #include <sstream>
+#include <assert.h>
 
 typedef std::string vertex;
 typedef std::pair<vertex, vertex> edge;
@@ -73,4 +74,58 @@ bool Graph::isAdjacent(vertex a, vertex b) {
 
 bool Graph::hasVertex(vertex v) {
     return this->ptr->vertices.find(v) != this->ptr->vertices.end();
+}
+
+void Graph::flipEdge(vertex a, vertex b) {
+    if (!this->ptr->directed || !isAdjacent(a, b)) return;
+
+    float edgeValue = this->ptr->weighted ? getEdgeValue(a, b) : 0;
+    removeEdge(a, b);
+    addEdge(a, b, edgeValue);
+}
+
+void Graph::addVertex(vertex v) {
+    if (hasVertex(v)) return;
+
+    this->ptr->vertices.insert(v);
+    this->ptr->neighborsMap[v] = vertex_set();
+}
+
+void Graph::setEdgeValue(vertex a, vertex b, float edgeValue) {
+    assert(this->ptr->weighted);
+    assert(isAdjacent(a, b));
+
+    this->ptr->edgeValueMap[edge(a, b)] = edgeValue;
+    if (!this->ptr->directed) this->ptr->edgeValueMap[edge(b, a)] = edgeValue;
+}
+
+void Graph::addEdge(vertex a, vertex b, float edgeValue = 0) {
+    addVertex(a);
+    addVertex(b);
+
+    this->ptr->neighborsMap[a].insert(b);
+    if (!this->ptr->directed) this->ptr->neighborsMap[b].insert(a);
+
+    if (!this->ptr->weighted) return;
+
+    setEdgeValue(a, b, edgeValue);
+}
+
+void Graph::removeEdge(vertex a, vertex b) {
+    if (!isAdjacent(a, b)) return;
+
+    this->ptr->neighborsMap[a].erase(b);
+    this->ptr->edgeValueMap.erase(edge(a, b));
+
+    if (!this->ptr->directed) {
+        this->ptr->neighborsMap[b].erase(a);
+        this->ptr->edgeValueMap.erase(edge(b, a));
+    }
+}
+
+float Graph::getEdgeValue(vertex a, vertex b) {
+    assert(this->ptr->weighted);
+    assert(isAdjacent(a, b));
+
+    return this->ptr->edgeValueMap[edge(a, b)];
 }
