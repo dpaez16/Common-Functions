b4bded6951c22c77a00108b5064a047498f73e50
Danny Paez <dpaez16@yahoo.com>
2021-05-29 17:35:38 -0700

    Compressed APIS to single class

--- C++/fibheap.cpp
+++ C++/fibheap.cpp
@@ -3,58 +3,16 @@
 #include <unordered_map>
 #include <assert.h>
 
-class LinkedList {
-public:
-    typedef struct LLNode {
-        LLNode * prev;
-        LLNode * next;
-        void * data;
-    } LLNode;
-
-    LinkedList() {
-        this->n = 0;
-        this->head = NULL;
-        this->tail = NULL;
-        this->minElem = NULL;
-    };
-
-    ~LinkedList() {
-        if (this->n == 0) return;
-
-        LLNode * ptr = this->head;
-        while (ptr != NULL) {
-            LLNode * next = ptr->next;
-            delete ptr;
-            ptr = next;
-        }
-    };
-
-    LLNode * push(std::string, float key);
-    LLNode * pushNode(LLNode * node);
-    LLNode * remove(LLNode * node);
-    LLNode * pop();
-    void merge(const LinkedList & other);
-    LLNode * min() {
-        assert(size() > 0);
-        return this->minElem;
-    };
-
-    size_t size() {
-        return this->n;
-    };
-
-private:
-    size_t n;
-    LLNode * minElem;
-    LLNode * head;
-    LLNode * tail;
-};
 
 typedef struct FibNode {
     std::string elem;
     float key;
     FibNode * parent;
-    LinkedList childList;
+    FibNode * next;
+    FibNode * prev;
+    FibNode * childHead;
+    FibNode * childTail;
+    size_t rank;
     bool marked;
 } FibNode;
 
@@ -62,7 +20,8 @@ struct FibonacciHeap::ClassVars {
     size_t size;
     bool reverse;
     FibNode * minNode;
-    LinkedList rootList;
+    FibNode * rootHead;
+    FibNode * rootTail;
     std::unordered_map<std::string, FibNode *> nodeMap;
 };
 
@@ -71,6 +30,42 @@ FibonacciHeap::FibonacciHeap(bool reverse) {
     this->ptr->size = 0;
     this->ptr->reverse = reverse;
     this->ptr->minNode = NULL;
-    this->ptr->rootList = LinkedList();
+    this->ptr->rootHead = NULL;
+    this->ptr->rootTail = NULL;
     this->ptr->nodeMap = std::unordered_map<std::string, FibNode *>();
+}
+
+void FibonacciHeap::push(std::string elem, float key) {
+    FibNode * node = new FibNode;
+    node->elem = elem;
+    node->key = key;
+    node->parent = NULL;
+    node->prev = NULL;
+    node->next = NULL;
+    node->childHead = NULL;
+    node->childTail = NULL;
+    node->rank = 0;
+    node->marked = false;
+
+    this->ptr->size++;
+    if (this->ptr->size == 1) {
+        this->ptr->rootHead = node;
+        this->ptr->rootTail = node;
+        this->ptr->minNode = node;
+	return;
+    }
+
+    this->ptr->rootTail->next = node;
+    node->prev = this->ptr->rootTail;
+    this->ptr->rootTail = node;
+}
+
+std::string FibonacciHeap::top() {
+    assert(size() > 0);
+
+    return this->ptr->minNode->elem;
+}
+
+size_t FibonacciHeap::size() {
+    return this->ptr->size;
 }
