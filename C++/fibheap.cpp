c38431eaf1186081d7de8d093f32ec5fa6c1b02d
Danny Paez <dpaez16@yahoo.com>
2021-05-30 19:04:05 -0700

    pop in progress

--- C++/fibheap.cpp
+++ C++/fibheap.cpp
@@ -50,8 +50,19 @@ FibNode * createFibNode(std::string elem, float key) {
     return node;
 }
 
+FibNode * getMinNode(FibNode *& a, FibNode *& b, bool reverse) {
+    if (reverse) {
+        return a->key < b->key ? b : a;
+    }
+
+    return a->key < b->key ? a : b;
+}
+
 void FibonacciHeap::push(std::string elem, float key) {
+    assert(this->ptr->nodeMap.find(elem) == this->ptr->nodeMap.end());
+
     FibNode * node = createFibNode(elem, key);
+    this->ptr->nodeMap[elem] = node;
 
     this->ptr->size++;
     if (this->ptr->size == 1) {
@@ -64,6 +75,8 @@ void FibonacciHeap::push(std::string elem, float key) {
     this->ptr->rootTail->next = node;
     node->prev = this->ptr->rootTail;
     this->ptr->rootTail = node;
+
+    this->ptr->minNode = getMinNode(this->ptr->minNode, node, this->ptr->reverse);
 }
 
 std::string FibonacciHeap::top() {
@@ -78,4 +91,76 @@ size_t FibonacciHeap::size() {
 
 bool FibonacciHeap::empty() {
     return this->ptr->size == 0;
+}
+
+FibNode * findMinNode(FibNode *& head, bool reverse) {
+    assert(head != NULL);
+
+    FibNode * ptr = head;
+    FibNode * minNode = head;
+
+    while (ptr != NULL) {
+        minNode = getMinNode(ptr, minNode, reverse);
+        ptr = ptr->next;
+    }
+
+    return minNode;
+}
+
+void consolidate(FibNode *& rootHead, FibNode *& rootTail, bool reverse) {
+    std::unordered_map<size_t, FibNode *> rankMap;
+    FibNode * ptr = rootHead;
+
+    while (ptr != NULL) {
+        size_t rank = ptr->rank;
+        if (rankMap.find(rank) == rankMap.end()) {
+            rankMap[rank] = ptr;
+            ptr = ptr->next;
+        } else if (rankMap[rank] == ptr) {
+            ptr = ptr->next;
+        } else {
+            FibNode * otherNode = rankMap[rank];
+            std::pair<FibNode *, FibNode *> nodes = getNodes(ptr, otherNode, reverse);
+            FibNode * mainNode = nodes.first;
+            FibNode * childNode = nodes.second;
+            remove(childNode);
+            childNode->parent = mainNode;
+            mainNode->rank++;
+
+            rankMap[rank] = NULL;
+            ptr = rootHead;
+        }
+    }
+}
+
+void FibonacciHeap::pop() {
+    assert(!empty());
+
+    std::string elem = top();
+    this->ptr->size--;
+    this->ptr->nodeMap.erase(elem);
+
+    if (this->ptr->size == 0) {
+        this->ptr->rootHead = NULL;
+        this->ptr->rootTail = NULL;
+        this->ptr->minNode = NULL;
+        return;
+    }
+
+    FibNode * minNode = this->ptr->minNode;
+    FibNode * childHead = minNode->childHead;
+    FibNode * childTail = minNode->childTail;
+
+    if (childHead == NULL) return;
+
+    FibNode * ptr = childHead;
+    while (ptr != NULL) {
+        ptr->parent = NULL;
+        ptr = ptr->next;
+    }
+
+    this->ptr->rootTail->next = childHead;
+    this->ptr->rootTail = childTail;
+    consolidate(this->ptr->rootHead, this->ptr->rootTail, this->ptr->reverse);
+    this->ptr->minNode = findMinNode(this->ptr->rootHead, this->ptr->reverse);
 }
--- C++/fibheap.h
+++ C++/fibheap.h
@@ -10,7 +10,7 @@ public:
     //FibonacciHeap& operator=(const FibonacciHeap &);
     void push(std::string elem, float key);
     std::string top();
-    //void pop();
+    void pop();
     //void decreaseKey(std::string elem, float newKey);
     size_t size();
     bool empty();
