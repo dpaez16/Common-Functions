85f9485ca8684618f3059aabd01334402e9b2b3c
Danny Paez <dpaez16@yahoo.com>
2021-06-08 20:28:15 -0700

    dijkstra test

--- C++/tests/graph_test.cpp
+++ C++/tests/graph_test.cpp
@@ -22,8 +22,19 @@ TEST(Graph, DijkstraTest) {
 
     pair<unordered_map<string, float>, unordered_map<string, string>> p = g.dijkstra("a");
     unordered_map<string, float> dist = p.first;
+    unordered_map<string, string> prev = p.second;
     
     ASSERT_EQ(dist["a"], 0);
+    ASSERT_EQ(dist["b"], 3);
+    ASSERT_EQ(dist["c"], 2);
+    ASSERT_EQ(dist["d"], 5);
+    ASSERT_EQ(dist["e"], 6);
+
+    ASSERT_TRUE(prev.find("a") == prev.end());
+    ASSERT_EQ(prev["b"], "c");
+    ASSERT_EQ(prev["c"], "a");
+    ASSERT_EQ(prev["d"], "b");
+    ASSERT_EQ(prev["e"], "b");
 }
 
 int main(int argc, char ** argv) {
