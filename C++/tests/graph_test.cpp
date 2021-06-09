78cac760fb0ada9ce3c10be43c2fa2cdab24b341
Danny Paez <dpaez16@yahoo.com>
2021-06-09 12:25:09 -0700

    bellman-ford test

--- C++/tests/graph_test.cpp
+++ C++/tests/graph_test.cpp
@@ -37,6 +37,42 @@ TEST(Graph, DijkstraTest) {
     ASSERT_EQ(prev["e"], "b");
 }
 
+TEST(Graph, BellmanFordTest) {
+    Graph g(true, true);
+
+    g.addEdge("s", "e", 8);
+    g.addEdge("s", "a", 10);
+
+    g.addEdge("e", "d", 1);
+
+    g.addEdge("d", "a", -4);
+    g.addEdge("d", "c", -1);
+
+    g.addEdge("c", "b", -2);
+
+    g.addEdge("b", "a", 1);
+
+    g.addEdge("a", "c", 2);
+
+    pair<unordered_map<string, float>, unordered_map<string, string>> p = g.bellmanFord("s");
+    unordered_map<string, float> dist = p.first;
+    unordered_map<string, string> prev = p.second;
+
+    ASSERT_EQ(dist["s"], 0);
+    ASSERT_EQ(dist["a"], 5);
+    ASSERT_EQ(dist["b"], 5);
+    ASSERT_EQ(dist["c"], 7);
+    ASSERT_EQ(dist["d"], 9);
+    ASSERT_EQ(dist["e"], 8);
+
+    ASSERT_TRUE(prev.find("s") == prev.end());
+    ASSERT_EQ(prev["a"], "d");
+    ASSERT_EQ(prev["b"], "c");
+    ASSERT_EQ(prev["c"], "a");
+    ASSERT_EQ(prev["d"], "e");
+    ASSERT_EQ(prev["e"], "s");
+}
+
 int main(int argc, char ** argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
