a670d4a141cd37f0df3ba370e7190f4923c81bbd
Danny Paez <dpaez16@yahoo.com>
2021-06-10 14:52:58 -0700

    insert test for trie

--- C++/tests/trie_test.cpp
+++ C++/tests/trie_test.cpp
@@ -6,6 +6,21 @@ using namespace std;
 
 TEST(Trie, InsertTest) {
 	Trie t;
+    vector<string> words = {"cat", "cats", "char"};
+    for (string word : words) {
+        t.insert(word);
+    }
+
+    vector<string> query = t.query("ca");
+    unordered_set<string> actualQuery = {"cat", "cats"};
+
+    ASSERT_EQ(query.size(), actualQuery.size());
+    for (string word : query) {
+        ASSERT_TRUE(actualQuery.find(word) != actualQuery.end());
+    }
+
+    query = t.query("d");
+    ASSERT_EQ(query.size(), 0);
 }
 
 int main(int argc, char ** argv) {
