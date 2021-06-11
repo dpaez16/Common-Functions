224ab7e073c8689862f8fba084d57eadfb78b2c6
Danny Paez <dpaez16@yahoo.com>
2021-06-11 16:52:38 -0700

    another try

--- C++/tests/get_cov.sh
+++ C++/tests/get_cov.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+[ -d "coverage" ] && rm -rf coverage
+mkdir coverage
+
 make gcov
+obj_files=$(ls ../ | grep ".\.o")
 
-cd ../
+for filename in $obj_files
+do
+    base_filename=$(echo $filename | tr -d ".o")
+    mv $base_filename.cpp.gcov coverage/.
+done
 
-lcov -c --directory . --output-file cov.info
+make clean
+
+for filename in $(ls coverage)
+do
+    mv coverage/$filename .
+done
+
+rmdir coverage
